83cb3907c20d1a4ed7930700b51923b2579773c8
Elshater <elshater.ehab@gmail.com>
2023-08-11 08:52:33 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,8 +2,7 @@
 
 /**
  *prints all possible combinations of single-digit numbers
- * main -Entry point
- * Descrption:'Prints numbers between 0 to 9 with commas and spaces.'
+ * main - Prints numbers between 0 to 9 with commas and spaces.
  *
  * Return: Always 0 (Success)
  */
