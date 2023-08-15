fd40dfc182a1c0c67d8cada06b8a369125f1daaa
Elshater <elshater.ehab@gmail.com>
2023-08-15 07:43:19 -0700

    3.islower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main excutable file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	c='t';
+
+	_islower(c);
+	_putchar('\n');
+	return (0);
+}
