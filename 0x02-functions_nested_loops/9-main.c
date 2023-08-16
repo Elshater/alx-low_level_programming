3cf9640b0a94dcd4366d808dd122670ed22828af
Elshater <elshater.ehab@gmail.com>
2023-08-16 00:59:59 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main excutable file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	times_table();
+
+	_putchar('\n');
+	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int i, j, multi;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 0; j <= 9; j++)
+		{
+			multi = i * j;
+			_putchar(',');
+			_putchar(' ');
+
+
+			if (multi <= 9)
+			{
+				_putchar(',');
+				_putchar(multi + 48);
+			}
+			else
+			{
+				_putchar((multi / 10) + 48);
+				_putchar((multi % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
