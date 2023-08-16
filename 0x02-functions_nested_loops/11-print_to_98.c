29222090b15ada8ce861d08bbb282e0eebd9c93c
Elshater <elshater.ehab@gmail.com>
2023-08-16 02:48:12 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers
+ * from n to 98.
+ *
+ * @n: input number.
+ *
+ * Return: no return.
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i > 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("%d, ", n);
+
+}
