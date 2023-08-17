d811b265a8b09b211fc506b560066271d00e4c17
Elshater <elshater.ehab@gmail.com>
2023-08-17 06:24:02 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,28 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_diagonal - prints diagonal line n times.
- * @n: times diagonal line is printed.
- * Return: no return.
+ * print_diagonal - prints diagonal stuff
+ *
+ * @n: integer to set diagonal
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j < i; j++)
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-		}
-		_putchar('\');
-		if (i < (n - 1))
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
 			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
