946391cb83401b1cc786a1763fb419504b2f2090
Elshater <elshater.ehab@gmail.com>
2023-09-05 12:26:00 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,17 +11,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
-	unsigned int i, j, k, l;
-
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
-		s1="";
+		s1 = "";
 	if (s2 == NULL)
-		s2="";
+		s2 = "";
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; j !='\0'; j++)
+
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	strout = malloc(sizeof(char) * (i + j + 1));
@@ -32,12 +32,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-
 	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
 
-	l = j;
-	for (j = 0; j < l; j++)
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
 	return (strout);
